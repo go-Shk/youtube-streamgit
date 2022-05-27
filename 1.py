931b7dd69a70abfb568b13276cfe955459750feb
go-Shk <106368084+go-Shk@users.noreply.github.com>
2022-05-27 19:35:16 +0900

    1st commit

--- 1.py
+++ 1.py
@@ -1,16 +1,15 @@
-
-from click import option
 import streamlit as st
 import time
 
 
-st.title('streamlit 超入門')
+st.title('Streamlit 超入門')
 
 st.write('プログレスバーの表示')
 'Start!!'
 
 latest_iteration = st.empty()
 bar = st.progress(0)
+
 for i in range(100):
     latest_iteration.text(f'Interation {i+1}')
     bar.progress(i + 1)
